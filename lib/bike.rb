0ad1b080cad70dee9dd17e160df16189a5a75218
Mike Gehard <mike.gehard@gmail.com>
2014-03-10 11:49:42 -0600

    Make color method more explicit
    There was some confusion about where the color method was
    coming from

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,11 +1,15 @@
 class Bike
-  attr_reader :color
-
   def initialize(color)
     @color = color
   end
 
   def is_cool?
     color == "red"
+  end
+
+  # This is the same as:
+  # attr_reader :color
+  def color
+    @color
   end
 end
