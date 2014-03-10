f9b537dcb252e8ee26f643e95b71774a5cbbca72
Mike Gehard <mike.gehard@gmail.com>
2014-03-10 11:59:59 -0600

    Switch over to using Minitest::Test

--- spec/bike_test.rb
+++ spec/bike_test.rb
@@ -1,52 +1,43 @@
 $LOAD_PATH.unshift(File.expand_path('lib'), __FILE__)
 
 require 'bike'
+require "minitest/autorun"
 
-def assert_equal(expected, actual)
-  if expected == actual
-    print '.'
-  else
-    raise "Test failed. Expected #{expected} to equal #{actual}"
+class TestBike < Minitest::Test
+  def test_ask_bike_for_color
+    #set up
+    bike = Bike.new("red")
+
+    # expected value
+    expected_color = "red"
+
+    #actaul value
+    actual_color = bike.color
+
+    assert_equal(expected_color, actual_color)
+  end
+
+  def test_red_bikes_are_cool
+    #set up
+    bike = Bike.new("red")
+
+    expected_value = true
+
+    #actaul value
+    actual_value = bike.is_cool?
+
+    assert_equal(expected_value, actual_value)
+  end
+
+  def test_blue_bikes_are_not_cool
+    #set up
+    bike = Bike.new("blue")
+
+    expected_value = false
+
+    #actaul value
+    actual_value = bike.is_cool?
+
+    assert_equal(expected_value, actual_value)
   end
 end
-
-def test_ask_bike_for_color
-  #set up
-  bike = Bike.new("red")
-
-  # expected value
-  expected_color = "red"
-
-  #actaul value
-  actual_color = bike.color
-
-  assert_equal(expected_color, actual_color)
-end
-
-def test_red_bikes_are_cool
-  #set up
-  bike = Bike.new("red")
-
-  expected_value = true
-
-  #actaul value
-  actual_value = bike.is_cool?
-
-  assert_equal(expected_value, actual_value)
-end
-
-def test_blue_bikes_are_not_cool
-  #set up
-  bike = Bike.new("blue")
-
-  expected_value = false
-
-  #actaul value
-  actual_value = bike.is_cool?
-
-  assert_equal(expected_value, actual_value)
-end
-
-test_ask_bike_for_color
-test_red_bikes_are_cool
-test_blue_bikes_are_not_cool
