9b1c00361e910de939dcd734225ac6803d6cd95c
Mike Gehard <mike.gehard@gmail.com>
2014-03-11 13:01:49 -0600

    Allow user to delete a key

--- spec/key_value_store_spec.rb
+++ spec/key_value_store_spec.rb
@@ -11,4 +11,16 @@ describe "KeyValueStore" do
 
     expect(actual_value).to eq expected_value
   end
+
+  it "allow for deletion of a single key" do
+    key_value_store = KeyValueStore.new
+    key_value_store.add("foo", 1000)
+
+    key_value_store.delete('foo')
+
+    expected_value = nil
+    actual_value = key_value_store.get('foo')
+
+    expect(actual_value).to eq expected_value
+  end
 end
